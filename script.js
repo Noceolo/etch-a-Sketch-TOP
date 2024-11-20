18009585540ed961279a79dd589f4443ee1dcddf
Noceolo <eliottlm@hotmail.fr>
2024-11-20 11:02:34 +0100

    added random color selector (r g b randomizer)

--- script.js
+++ script.js
@@ -1,65 +1,54 @@
 const container = document.querySelector("#container")
 const sizeBtn = document.querySelector("#sizeSelect")
 
-for(x=0; x<256;x++) {
-    var board = document.createElement('div');
-    board.className = "grid";
-    container.appendChild(board);
-    }
-    document.querySelectorAll('.grid').forEach(item => {
-        item.addEventListener('mouseover', (event) => {
-            event.target.style.backgroundColor = "green";
-            setTimeout(() => {
-                event.target.style.backgroundColor = "aqua";
-            }, 1000);
-        },
-        false,
-      )
-    });
 
-    function sizePrompt (){
-        var sizePrompt = parseInt(window.prompt("How many square per side ? Select between 1 and 100"), 10)
-        if ( /^[0-9.,]+$/.test(sizePrompt) && sizePrompt <= 100) {
-            console.log('pass');
-            clear();
+function sizePrompt (){
+    var sizePrompt = parseInt(window.prompt("How many square per side ? Select between 1 and 100"), 10)
+    if ( /^[0-9.,]+$/.test(sizePrompt) && sizePrompt <= 100) {
+        console.log('pass');
+        clear();
             
-            createGrid(sizePrompt);
-          } else {
+        createGrid(sizePrompt);
+        } else {
             alert("You need to enter a number between 1 and 100 !");
-          }
+        }
         
     };
 
-    sizeBtn.addEventListener('click', () => {
-        sizePrompt();
-    })
+sizeBtn.addEventListener('click', () => {
+    sizePrompt();
+})
 
 
-    function clear() {
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
+function clear() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
+}
 
-    function createGrid(size) {
-        const totalSize = 960; 
-        const cellSize = totalSize / size;
+function createGrid(size) {
+    const totalSize = 480; 
+    const cellSize = totalSize / size;
 
-        for (let i = 0; i< size * size; i++){
-            const cell = document.createElement("div")
-            cell.className = "grid";
+    for (let i = 0; i< size * size; i++){
+        const cell = document.createElement("div")
+        cell.className = "grid";
 
-            cell.style.width = `${cellSize}px` ;
-            cell.style.height = `${cellSize}px` ;
-            cell.style.backgroundColor = "aqua";
-            container.appendChild(cell); 
+        cell.style.width = `${cellSize}px` ;
+        cell.style.height = `${cellSize}px` ;
+        cell.style.backgroundColor = "white";
+        container.appendChild(cell); 
 
-            cell.addEventListener('mouseover', () => {
-                cell.style.backgroundColor = "green";
-                setTimeout(() => {
-                    cell.style.backgroundColor = "aqua";
-                }, 1000);
-            });
-        }
+        cell.addEventListener('mouseover', () => {
+                 const randomR = Math.floor(Math.random() * 256)
+                const randomG = Math.floor(Math.random() * 256)
+                const randomB = Math.floor(Math.random() * 256) 
+                cell.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;;
+            setTimeout(() => {
+                cell.style.backgroundColor = "white";
+            }, 1000);
+        });
     }
-        
+}
+
+createGrid(40);
